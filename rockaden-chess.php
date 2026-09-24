0475897b5011ca4e23509a2315362553098ca139
msvens <msvens@gmail.com>
2026-09-24 09:53:57 +0200

    Bump version to 0.48.0

--- rockaden-chess.php
+++ rockaden-chess.php
@@ -3,7 +3,7 @@
  * Plugin Name: Chess Club Manager for schack.se
  * Plugin URI:  https://github.com/msvens/chess-wp-plugin
  * Description: Training management, calendar, tournaments and schack.se (SSF) integration for Swedish chess clubs.
- * Version:     0.47.1
+ * Version:     0.48.0
  * Author:      Martin Svensson
  * Author URI:  https://github.com/msvens/chess-wp-plugin
  * Text Domain: rockaden-chess
@@ -20,7 +20,7 @@ defined( 'ABSPATH' ) || exit;
 
 define( 'RC_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'RC_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-define( 'RC_VERSION', '0.47.1' );
+define( 'RC_VERSION', '0.48.0' );
 
 // GitHub-based update checker. Reads release assets from the chess-wp-plugin
 // repository and lets WordPress show the standard "update available" UI in
